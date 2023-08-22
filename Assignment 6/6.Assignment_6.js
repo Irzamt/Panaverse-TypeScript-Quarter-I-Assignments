3b47f580c28b93802477e760f6ed0344787d7ca1
Irzam Tahir <irzamtahir4@gmail.com>
2023-08-22 22:19:41 +0500

    Completed Task 1

--- Assignment 6/6.Assignment_6.js
+++ Assignment 6/6.Assignment_6.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("hello");
+//1 - Develop a program that calculates and prints the sum of the first n even numbers using a for loop.
+let n = 100;
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+        sum += i;
+    }
+}
+console.log(`Sum of first ${n} even numbers: ${sum}`);
--- Assignment 6/6.Assignment_6.ts
+++ Assignment 6/6.Assignment_6.ts
@@ -1,1 +1,10 @@
-console.log("hello");
+//1 - Develop a program that calculates and prints the sum of the first n even numbers using a for loop.
+let n:number = 100;
+let sum:number = 0;
+for (let i:number = 1; i <= n; i++){
+    if (i % 2 === 0){
+        sum += i;
+    }
+}
+
+console.log(`Sum of first ${n} even numbers: ${sum}`);
