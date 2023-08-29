4bdd9574c604208f12897106d93707f6e505a7c7
Irzam Tahir <irzamtahir4@gmail.com>
2023-08-30 01:50:12 +0500

    Done Task 7

--- Assignment 7/Assignment_7.ts
+++ Assignment 7/Assignment_7.ts
@@ -53,5 +53,9 @@ console.log(updatedArr6);
 
 // 7 - Given an array of names ["Alice", "Bob", "Charlie", "David", "Emily"], use the forEach method to log each name with an exclamation mark at the end, e.g., "Alice!".
 /********************************************************************************************************************** */
+let names:string[] = ["Alice", "Bob", "Charlie", "David", "Emily"];
 
+names.forEach(n => {
+    console.log(n + "!");
+});
 /********************************************************************************************************************** */
