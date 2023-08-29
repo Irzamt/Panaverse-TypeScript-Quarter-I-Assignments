9ee86a39cb5cafa9f39649e50f2f9b678629530c
Irzam Tahir <irzamtahir4@gmail.com>
2023-08-30 01:14:42 +0500

    Completed task 1

--- Assignment 7/Assignment_7.ts
+++ Assignment 7/Assignment_7.ts
@@ -1,9 +1,18 @@
 // 1 - Write a program that uses filter to remove all negative numbers from an array of numbers
 /********************************************************************************************************************** */
+let arry: number[] = [12, 56 ,48, -79, 52, -96, -65, 2, -61]
 
+let updatedArr:number[] = arry.filter ((n:number) => {
+    if (n > 0)
+        return n;
+})
+console.log(updatedArr);
 /********************************************************************************************************************** */
+
 // 2 - Given an array of numbers [1, 2, 3, 4, 5], use the map method to create a new array where each number is multiplied by 2.
 /********************************************************************************************************************** */
+let array2:number[] = [1,2,3,4,5];
+
 
 /********************************************************************************************************************** */
 // 3 - Given an array of strings ["apple", "banana", "cherry", "date", "grape"], use the filter method to create a new array containing only the fruits with more than 5 characters.0
