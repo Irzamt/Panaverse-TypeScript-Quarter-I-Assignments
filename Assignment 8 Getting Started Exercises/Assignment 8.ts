b2c909ae132f79115178e4239799542141448795
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-08 03:26:53 +0500

    Done 5

--- Assignment 8 Getting Started Exercises/Assignment 8.ts
+++ Assignment 8 Getting Started Exercises/Assignment 8.ts
@@ -21,12 +21,22 @@
 
 // 4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:  Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 /********************************************************************************************************************* */
+// let quote:string = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”';
 
+// console.log(quote);
+/********************************************************************************************************************* */
 
 // 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+/********************************************************************************************************************* */
+// let famous_person: string = "Albert Einstein";
+// let message: string = '“A person who never made a mistake never tried anything new.”';
 
-// 6. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each
-// character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+// console.log(`${famous_person} once said, ${message}`);
+/********************************************************************************************************************* */
+
+// 6. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+/********************************************************************************************************************* */
+
 
 // 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
 
