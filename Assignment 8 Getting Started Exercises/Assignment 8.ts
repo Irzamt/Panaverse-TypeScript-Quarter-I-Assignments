9c8063ad7454e0887a6612c1a49bf4bd48b50e4a
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-09 02:09:05 +0500

    done 16

--- Assignment 8 Getting Started Exercises/Assignment 8.ts
+++ Assignment 8 Getting Started Exercises/Assignment 8.ts
@@ -179,35 +179,42 @@ for (let i:number = 0; i < guests2.length; i++){
     updatedGuestList.push(guests2[i].guestName);
 }
 
-for (let i: number = 0; i < updatedGuestList.length; i++)
-    console.log(updatedGuestList[i], "you are invited to dinner @ 8:00 PM Saturday Night!!");
+// for (let i: number = 0; i < updatedGuestList.length; i++)
+    // console.log(updatedGuestList[i], "you are invited to dinner @ 8:00 PM Saturday Night!!");
         
 
 /********************************************************************************************************************* */
 
-// 16. More Guests: You just found a bigger dinner table, so now more space is
-// available. Think of three more guests to invite to dinner.
+// 16. More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
 
-// • Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a
-// bigger dinner table.
-
+// • Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
 // • Add one new guest to the beginning of your array.
-
 // • Add one new guest to the middle of your array.
 // • Use append() to add one new guest to the end of your list.
 // • Print a new set of invitation messages, one for each person in your list.
+/********************************************************************************************************************* */
+console.log("I  found a bigger table");
+
+updatedGuestList.unshift("Raza"); //Added at the begining
+updatedGuestList.push("Zaini"); //
+updatedGuestList.splice(updatedGuestList.length/2, 0, "Farhan");
+
+for (let i: number = 0; i < updatedGuestList.length; i++)
+    console.log(updatedGuestList[i], "you are invited to dinner @ 8:00 PM Saturday Night!!");
+
+
+/********************************************************************************************************************* */
 
 // 17. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
 
 // • Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite only two people for dinner.
-
-// • Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print
-// a message to that person letting them know you’re sorry you can’t invite them to dinner.
-
+// • Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
 // • Print a message to each of the two people still on your list, letting them know they’re still invited.
+// • Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+/********************************************************************************************************************* */
 
-// • Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end
-// of your program.
+
+/********************************************************************************************************************* */
 
 // 18. Seeing the World: Think of at least five places in the world you’d like to visit.
 
