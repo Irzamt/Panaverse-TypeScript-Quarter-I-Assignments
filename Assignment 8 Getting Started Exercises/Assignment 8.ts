48bc21f9675f1c9891806f190d15c4511a73771b
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-12 02:38:54 +0500

    done 34

--- Assignment 8 Getting Started Exercises/Assignment 8.ts
+++ Assignment 8 Getting Started Exercises/Assignment 8.ts
@@ -705,14 +705,14 @@ console.log(car == 'subaru')
 • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
 • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
 ***************************************************************************************************************************/
-const myPizza:string[] = ['tikka', 'fajita', 'pepperoni'];
+// const myPizza:string[] = ['tikka', 'fajita', 'pepperoni'];
 
-myPizza.forEach(n => {
-    console.log(`I like ${n}!!`);
-});
-console.log(`I can't resist the deliciousness of Pepperoni.`);
-console.log(`Margherita's simplicity is a culinary masterpiece.`);
-console.log(`The sweetness of Hawaiian pizza is simply amazing.`);
+// myPizza.forEach(n => {
+//     console.log(`I like ${n}!!`);
+// });
+// console.log(`I can't resist the deliciousness of Pepperoni.`);
+// console.log(`Margherita's simplicity is a culinary masterpiece.`);
+// console.log(`The sweetness of Hawaiian pizza is simply amazing.`);
 
 /*********************************************************************************************************************
 35. Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
