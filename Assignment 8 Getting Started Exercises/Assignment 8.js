96932e1490a45976eaf696c0d2aa465c5fcd623f
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-12 03:45:47 +0500

    done 43

--- Assignment 8 Getting Started Exercises/Assignment 8.js
+++ Assignment 8 Getting Started Exercises/Assignment 8.js
@@ -671,13 +671,25 @@ Call your function with at least three city-country pairs, and print the value t
 /*********************************************************************************************************************
 42. Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
 ***************************************************************************************************************************/
-function make_great(magician) {
-    magician.forEach(n => console.log(`Great Magician ${n}`));
-}
-make_great(["Raza", "Fahad", "Ahmad"]);
+// function make_great (magician:string[]){
+//     magician.forEach(n => console.log(`Great Magician ${n}`));
+// }
+// make_great (["Raza", "Fahad", "Ahmad"])
 /*********************************************************************************************************************
 43. Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
 ***************************************************************************************************************************/
+function show_magicians(magician) {
+    magician.forEach(n => console.log(n));
+}
+function make_great(magician) {
+    return magician.map(n => `Great Magician ${n}`);
+}
+const originalMagicians = ["Raza", "Fahad", "Ahmad"];
+const greatMagician = make_great([...originalMagicians]); // Create a copy of the original array
+console.log("Original Magicians:");
+show_magicians(originalMagicians);
+console.log("\nGreat Magicians:");
+show_magicians(greatMagician);
 /*********************************************************************************************************************
 44. Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
 ***************************************************************************************************************************/
--- Assignment 8 Getting Started Exercises/Assignment 8.ts
+++ Assignment 8 Getting Started Exercises/Assignment 8.ts
@@ -800,6 +800,23 @@ Call your function with at least three city-country pairs, and print the value t
 /*********************************************************************************************************************
 43. Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
 ***************************************************************************************************************************/
+// function show_magicians(magician: string[]) {
+//     magician.forEach(n => console.log(n));
+// }
+
+// function make_great(magician: string[]):string[] {
+//     return magician.map(n => `Great Magician ${n}`);
+// }
+
+// const originalMagicians: string[] = ["Raza", "Fahad", "Ahmad"];
+// const greatMagician: string[] = make_great([...originalMagicians]); // Create a copy of the original array
+
+// console.log("Original Magicians:");
+// show_magicians(originalMagicians);
+
+// console.log("\nGreat Magicians:");
+// show_magicians(greatMagician);
+
 
 /*********************************************************************************************************************
 44. Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
