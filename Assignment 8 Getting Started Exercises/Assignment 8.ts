708a1c9422dee228715443ab97040f6a0f91a3e7
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-12 03:06:44 +0500

    done 37

--- Assignment 8 Getting Started Exercises/Assignment 8.ts
+++ Assignment 8 Getting Started Exercises/Assignment 8.ts
@@ -728,16 +728,27 @@ console.log(car == 'subaru')
 /*********************************************************************************************************************
 36. T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
 ***************************************************************************************************************************/
-function make_shirt (size: number, text:string){
-    
-   return `The shirt is of size: ${size}\n"${text}"`;
-}
+// function make_shirt(size: string, text: string) {
+//     return `The shirt is of size: ${size}\n"${text}"`;
+// }
 
-console.log(make_shirt(45, "This is a great shirt"));
+// console.log(make_shirt("XL", "This is a great shirt"));
 
 /*********************************************************************************************************************
 37. Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
 ***************************************************************************************************************************/
+// function make_shirt(size: string = "Large", text: string = "I love TypeScript") {
+//     return `The shirt is of size: ${size}\n"${text}"`;
+// }
+
+// // Large shirt with the Default Message
+// console.log(make_shirt());
+
+// // Medium shirt with the Default Message
+// console.log(make_shirt("Medium"));
+
+// //Shirt of any size and different message
+// console.log(make_shirt("Small", "This is a great shirt"));
 
 /*********************************************************************************************************************
 38. Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.
